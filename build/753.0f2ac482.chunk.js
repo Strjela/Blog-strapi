b66a5b8e0dc6bd4ecea956625328f5b42e6b025b
Strjela <nikola.jelic44@gmail.com>
2024-07-01 10:53:25 +0200

    clean up

--- build/753.0f2ac482.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_project=self.webpackChunkmy_project||[]).push([[753],{30753:e=>{e.exports={}}}]);
--- build/9754.2ac1f5be.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_project=self.webpackChunkmy_project||[]).push([[9754],{29754:e=>{e.exports={}}}]);
